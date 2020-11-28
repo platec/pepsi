f214372ff5044d45a3ee57cabb1c0eecb392f4d2
Li Xiaoxun <open_source@126.com>
2020-11-28 16:17:55 +0800

    core对象

---
+++ src/pages/Editor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="editor" class="editor"></div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { fabric } from 'fabric';
+import { Pepsi } from '../pepsi';
+
+@Component
+export default class Editor extends Vue {
+  $refs!: {
+    editor: HTMLCanvasElement
+  }
+
+  mounted() {
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20
+    });
+    const pepsi = new Pepsi({
+      el: this.$refs.editor
+    });
+    // "add" rectangle onto canvas
+    pepsi.add(rect);
+  }
+}
+</script>
+<style lang="scss" scoped>
+.editor {
+  width: 100%;
+  height: 500px;;
+}
+</style>
---
+++ src/pepsi/core.ts
@@ -0,0 +1,55 @@
+import {
+  fabric
+} from 'fabric';
+import ResizeObserver from 'resize-observer-polyfill';
+
+export interface EditorOptions {
+  el: HTMLElement;
+}
+
+
+class Pepsi  {
+  private resizeObserver!: ResizeObserver | null;
+  options: EditorOptions;
+  canvasElement: HTMLCanvasElement
+  canvas: fabric.Canvas
+
+  constructor(options: EditorOptions) {
+    this.options = options;
+    this.canvasElement = document.createElement('canvas');
+    this.options.el.appendChild(this.canvasElement);
+    this.canvas = new fabric.Canvas(this.canvasElement);
+    // this.createResizeObserver();
+  }
+
+  add(data: fabric.Object) {
+    this.canvas.add(data);
+  }
+
+
+
+  createResizeObserver() {
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const {
+          width,
+          height
+        } = entry.contentRect;
+        console.log(width, height);
+        this.canvas.setWidth(width);
+        this.canvas.setHeight(height);
+      }
+    });
+
+    this.resizeObserver.observe(this.options.el);
+  }
+
+  destroyResizeObserver() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+      this.resizeObserver = null;
+    }
+  }
+}
+
+export default Pepsi;
